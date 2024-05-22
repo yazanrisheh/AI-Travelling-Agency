b5b898a7b8ff307987c9aa2019fdb3da559a413e
yazan <yazanrisheh@hotmail.com>
2024-05-22 13:26:05 +0400

    removed duplicate code

--- main.py
+++ main.py
@@ -80,90 +80,9 @@ class TripCrew:
         return result
 
 
-# This is the main function that you will use to run your custom crew.
-import os
-from crewai import Agent, Task, Crew, Process
-from langchain_openai import ChatOpenAI
-from dotenv import load_dotenv
-
-from textwrap import dedent
-from agents import TravelAgents
-from tasks import TravelTasks
-
-import streamlit as st
-
-load_dotenv()
-
 # This is the main class that you will use to define your custom crew.
 # You can define as many agents and tasks as you want in agents.py and tasks.py
 
-
-class TripCrew:
-    def __init__(self, origin, cities, starting_date, departure_date, interests, budget):
-        self.origin = origin
-        self.cities = cities
-        self.starting_date = starting_date
-        self.departure_date = departure_date
-        self.interests = interests
-        self.budget = budget
-
-    def run(self):
-        # Define your custom agents and tasks in agents.py and tasks.py
-        agents = TravelAgents()
-        tasks = TravelTasks()
-
-        # Define your custom agents and tasks here
-        expert_travel_agent = agents.expert_travel_agent()
-        city_selection_expert = agents.city_selection_expert()
-        local_tour_guide = agents.local_tour_guide()
-
-        # Custom tasks include agent name and variables as input
-        plan_itinerary = tasks.plan_itinerary(
-            expert_travel_agent,
-            self.cities,
-            self.starting_date,
-            self.departure_date,
-            self.interests,
-            self.budget
-        )
-
-        identify_city = tasks.identify_city(
-            city_selection_expert,
-            self.origin,
-            self.cities,
-            self.starting_date,
-            self.departure_date,
-            self.interests,
-            self.budget
-        )
-
-        gather_city_info = tasks.gather_city_info(
-            local_tour_guide,
-            self.cities,
-            self.starting_date,
-            self.departure_date,
-            self.interests,
-            self.budget
-        )
-
-        # Define your custom crew here
-        crew = Crew(
-            agents=[expert_travel_agent,
-                    city_selection_expert,
-                    local_tour_guide
-                    ],
-            tasks=[plan_itinerary,
-                    identify_city,
-                    gather_city_info
-                    ],
-            verbose=True,
-        )
-
-        result = crew.kickoff()
-        return result
-
-
-# This is the main function that you will use to run your custom crew.
 if __name__ == "__main__":
     st.title("RAAFYA International Travelling Agency ✈️")
     first_name = st.text_input(dedent("What is your first name? "))
